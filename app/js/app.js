99cb5f68ae1f756d0216291d006ce206d85937ce
Artem Aksentenko MAC <html@centrsvet.ru>
2022-05-22 13:31:09 +0300

    New features

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,28 @@
-import $ from 'jquery';
+import $ from "jquery";
 window.jQuery = $;
 window.$ = $;
 
 // import Swiper JS
-import Swiper, { Autoplay, Lazy } from 'swiper';
+import Swiper, { Autoplay, Lazy } from "swiper";
 Swiper.use([Autoplay, Lazy]);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const infoToogle = document.querySelector('.info-toogle');
-  const headerContacts = document.querySelector('.header-contacts');
-  const navToogle = document.querySelector('.nav-toogle');
-  const mainNav = document.querySelector('.main-nav');
-  const relativeLinks = document.querySelectorAll('.main-nav__link');
+document.addEventListener("DOMContentLoaded", () => {
+  const infoToogle = document.querySelector(".info-toogle");
+  const headerContacts = document.querySelector(".header-contacts");
+  const navToogle = document.querySelector(".nav-toogle");
+  const mainNav = document.querySelector(".main-nav");
+  const relativeLinks = document.querySelectorAll(".main-nav__link");
 
-  infoToogle.addEventListener('click', handleInfoToogle);
-  navToogle.addEventListener('click', handleNavToogle);
+  infoToogle.addEventListener("click", handleInfoToogle);
+  navToogle.addEventListener("click", handleNavToogle);
 
-  $("a[href^='#']").on('click', function () {
-    const _href = $(this).attr('href');
-    $('html, body').animate(
-      { scrollTop: $(_href).offset().top - 50 + 'px' },
+  $("a[href^='#']").on("click", function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate(
+      { scrollTop: $(_href).offset().top - 50 + "px" },
       {
         duration: 700,
-        easing: 'swing',
+        easing: "swing",
       }
     );
     handleNavToogle();
@@ -30,50 +30,65 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   relativeLinks.forEach(function (it) {
-    it.addEventListener('click', () => {
-      it.classList.toggle('active');
+    it.addEventListener("click", () => {
+      it.classList.toggle("active");
     });
   });
 
-  function handleInfoToogle() {
-    headerContacts.classList.toggle('open');
-    infoToogle.classList.toggle('open');
+  function removeAllActiveLinks() {
+    relativeLinks.forEach(function (it) {
+      it.classList.remove("active");
+    });
+  }
 
-    navToogle.classList.remove('open');
-    mainNav.classList.remove('open');
+  function handleInfoToogle() {
+    headerContacts.classList.toggle("open");
+    infoToogle.classList.toggle("open");
+
+    navToogle.classList.remove("open");
+    mainNav.classList.remove("open");
   }
 
   function handleNavToogle() {
-    navToogle.classList.toggle('open');
-    mainNav.classList.toggle('open');
+    navToogle.classList.toggle("open");
+    mainNav.classList.toggle("open");
 
-    headerContacts.classList.remove('open');
-    infoToogle.classList.remove('open');
+    headerContacts.classList.remove("open");
+    infoToogle.classList.remove("open");
   }
 
   function navBarScroll() {
     const navBarOffset = mainNav.offsetTop;
     let windowPosScroll = window.scrollY;
     if (window.screen.width > 960) {
-      window.addEventListener('scroll', function (evt) {
+      window.addEventListener("scroll", function (evt) {
         windowPosScroll = window.scrollY;
-        windowPosScroll >= navBarOffset ? mainNav.classList.add('scrolled') : mainNav.classList.remove('scrolled');
+        windowPosScroll >= navBarOffset ? mainNav.classList.add("scrolled") : mainNav.classList.remove("scrolled");
       });
     }
   }
+
+  document.addEventListener("click", (evt) => {
+    // handle close menu on click free space
+    if (evt.target?.classList.contains("main-nav__link")) {
+      return false;
+    } else removeAllActiveLinks();
+  });
+
   navBarScroll();
 
-  new Swiper('.swiper-container', {
+  new Swiper(".swiper-container", {
     // Optional parameters
     autoplay: {
       delay: 15000,
     },
-    direction: 'horizontal',
+    direction: "horizontal",
     preloadImages: true,
     lazy: {
       loadPrevNext: true,
     },
     loop: true,
     speed: 600,
+    grabCursor: true,
   });
 });
