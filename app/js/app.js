12cd0058dd66df720e5bee5e02e71d7b0aad5db2
Artem Aksentenko MAC <html@centrsvet.ru>
2022-05-29 19:45:35 +0300

    finish parts

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,30 @@
-import $ from 'jquery';
+import $ from "jquery";
 window.jQuery = $;
 window.$ = $;
 
 // import Swiper JS
-import Swiper, { Autoplay, Lazy, Navigation, Pagination } from 'swiper';
+import Swiper, { Autoplay, Lazy, Navigation, Pagination } from "swiper";
 Swiper.use([Autoplay, Lazy, Navigation, Pagination]);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const infoToogle = document.querySelector('.info-toogle');
-  const headerContacts = document.querySelector('.header-contacts');
-  const navToogle = document.querySelector('.nav-toogle');
-  const mainNav = document.querySelector('.main-nav');
-  const relativeLinks = document.querySelectorAll('.main-nav__link');
+import ScrollMagic from "scrollmagic";
 
-  infoToogle.addEventListener('click', handleInfoToogle);
-  navToogle.addEventListener('click', handleNavToogle);
+document.addEventListener("DOMContentLoaded", () => {
+  const infoToogle = document.querySelector(".info-toogle");
+  const headerContacts = document.querySelector(".header-contacts");
+  const navToogle = document.querySelector(".nav-toogle");
+  const mainNav = document.querySelector(".main-nav");
+  const relativeLinks = document.querySelectorAll(".main-nav__link");
 
-  $("a[href^='#']").on('click', function () {
-    const _href = $(this).attr('href');
-    $('html, body').animate(
-      { scrollTop: $(_href).offset().top - 50 + 'px' },
+  infoToogle.addEventListener("click", handleInfoToogle);
+  navToogle.addEventListener("click", handleNavToogle);
+
+  $("a[href^='#']").on("click", function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate(
+      { scrollTop: $(_href).offset().top - 50 + "px" },
       {
         duration: 700,
-        easing: 'swing',
+        easing: "swing",
       }
     );
     handleNavToogle();
@@ -30,67 +32,67 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   relativeLinks.forEach(function (it) {
-    it.addEventListener('click', () => {
-      it.classList.toggle('active');
+    it.addEventListener("click", () => {
+      it.classList.toggle("active");
     });
   });
 
   function removeAllActiveLinks() {
     relativeLinks.forEach(function (it) {
-      it.classList.remove('active');
+      it.classList.remove("active");
     });
   }
 
   function handleInfoToogle() {
-    headerContacts.classList.toggle('open');
-    infoToogle.classList.toggle('open');
+    headerContacts.classList.toggle("open");
+    infoToogle.classList.toggle("open");
 
-    navToogle.classList.remove('open');
-    mainNav.classList.remove('open');
+    navToogle.classList.remove("open");
+    mainNav.classList.remove("open");
   }
 
   function handleNavToogle() {
-    navToogle.classList.toggle('open');
-    mainNav.classList.toggle('open');
+    navToogle.classList.toggle("open");
+    mainNav.classList.toggle("open");
 
-    headerContacts.classList.remove('open');
-    infoToogle.classList.remove('open');
+    headerContacts.classList.remove("open");
+    infoToogle.classList.remove("open");
   }
 
   function navBarScroll() {
     const navBarOffset = mainNav.offsetTop;
     let windowPosScroll = window.scrollY;
     if (window.screen.width > 960) {
-      window.addEventListener('scroll', function (evt) {
+      window.addEventListener("scroll", function (evt) {
         windowPosScroll = window.scrollY;
-        windowPosScroll >= navBarOffset ? mainNav.classList.add('scrolled') : mainNav.classList.remove('scrolled');
+        windowPosScroll >= navBarOffset ? mainNav.classList.add("scrolled") : mainNav.classList.remove("scrolled");
       });
     }
   }
 
-  document.addEventListener('click', (evt) => {
+  document.addEventListener("click", (evt) => {
     // handle close menu on click free space
-    if (evt.target?.classList.contains('main-nav__link')) {
+    if (evt.target?.classList.contains("main-nav__link")) {
       return false;
     } else removeAllActiveLinks();
   });
 
   navBarScroll();
 
-  new Swiper('.swiper-container', {
+  new Swiper(".swiper-container", {
     // Optional parameters
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
+      el: ".swiper-pagination",
+      type: "progressbar",
     },
     autoplay: {
       delay: 45000,
     },
-    direction: 'horizontal',
+    direction: "horizontal",
     preloadImages: true,
     lazy: {
       loadPrevNext: true,
@@ -98,5 +100,14 @@ document.addEventListener('DOMContentLoaded', () => {
     loop: true,
     speed: 600,
     grabCursor: true,
+  });
+
+  // create a scene
+  const controller = new ScrollMagic.Controller();
+
+  // features
+  const aboutItems = document.querySelectorAll(".about__item");
+  aboutItems.forEach((item) => {
+    new ScrollMagic.Scene({ triggerElement: item, offset: -50 }).setClassToggle(item, "active").addTo(controller);
   });
 });
